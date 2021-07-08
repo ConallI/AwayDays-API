343a0a7b643d1de9a485d3fb2a9bb84918a111ae
Conall Imlah <conallimlah@gmail.com>
2021-07-08 23:45:30 +0900

    implement context

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+const { context } = require("./context");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context });
 
 const PORT = process.env.PORT || 4000;
 
