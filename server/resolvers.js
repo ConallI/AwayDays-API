21e9f89d4811f32a2a3abd81e819c934238dd8ab
Conall Imlah <conallimlah@gmail.com>
2021-07-08 19:29:57 +0900

    add basic resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+module.exports = {
+  Query: {
+    info: () => `This is the API for Away Days`,
+  },
+};
