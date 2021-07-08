67ede3d833ee72ebde6175749b1dcc53b40237df
Conall Imlah <conallimlah@gmail.com>
2021-07-08 22:37:20 +0900

    add seed data for all table

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,137 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import {PrismaClient, Prisma} from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput[] = [
+const cityData: Prisma.CityCreateInput[] = [
   {
-    name: 'Alice',
-    email: 'alice@prisma.io',
+    name: 'Sendai',
+    specialties: {
+      create: [
+        {
+          name: 'Gyutan',
+          description: 'Beef tongue, it sounds weird but it is suprisingly good!'
+        }
+      ]
+    },
+    sights: {
+      create: [
+        {
+          name: 'Statue of Date Masamune',
+          description: 'Statue of the famous founder of the city'
+        }
+      ]
+    },
+    teams: {
+      create: [
+        {
+          name: 'Vegalta Sendai',
+          league: {
+            create: [
+              {
+                name: 'Meiji Yasuda J1 League'
+              }
+            ]
+          },
+          sport: {
+            create: {
+              name: 'Football'
+            }
+          }
+        }
+      ]
+    },
+    region: {
+      create: {
+        name: 'Miyagi, Japan'
+      }
+    }
   },
   {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
+    name: 'Hiroshima',
+    specialties: {
+      create: [
+        {
+          name: 'Okonomiyaki',
+          description: 'Savory pancake style dish with many possible ingredients.'
+        }
+      ]
+    },
+    sights: {
+      create: [
+        {
+          name: 'Hiroshima Peace Memorial Museum',
+          description: 'Dedicated to documenting the atomic bombing of Hiroshima in World War II.'
+        }
+      ]
+    },
+    teams: {
+      create: [
+        {
+          name: 'Sanfrecce Hiroshima',
+          // league: {
+          //   create: [
+          //     {
+          //       name: 'Meiji Yasuda J1 League'
+          //     }
+          //   ]
+          // }
+        }
+      ]
+    },
+    region: {
+      create: {
+        name: 'Hiroshima, Japan'
+      }
+    }
   },
   {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
+    name: 'Tokyo',
+    sights: {
+      create: [
+        {
+          name: 'Tokyo SkyTree',
+          description: 'The tallest tower in the world'
+        },
+        {
+          name: 'Meiji Shrine',
+          description: 'A Shinto shrine in Shibuya, Tokyo, that is dedicated to the deified spirits of Emperor Meiji and his wife, Empress Shōken.'
+        }
+      ]
+    },
+    teams: {
+      create: [
+        {
+          name: 'FC Tokyo',
+          // league: {
+          //   create: [
+          //     {
+          //       name: 'Meiji Yasuda J1 League'
+          //     }
+          //   ]
+          // }
+        }
+      ]
+    },
+    region: {
+      create: {
+        name: 'Tokyo, Japan'
+      }
+    }
   },
-]
+];
 
-async function main() {
+async function seed() {
   console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
+  for (const city of cityData) {
+    const seedCity = await prisma.city.create({
+      data: city,
     })
-    console.log(`Created user with id: ${user.id}`)
+    console.log(`Created team with id: ${seedCity.id}`)
   }
-  console.log(`Seeding finished.`)
-}
+  console.log(`Seeding finished.`);
+};
 
-main()
+seed()
   .catch((e) => {
     console.error(e)
     process.exit(1)
@@ -36,3 +139,5 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
+
+  export default seed;
