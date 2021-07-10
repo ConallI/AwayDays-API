333f1c6058e0429ad07ff524acaf8ef2b1dcd658
Conall Imlah <conallimlah@gmail.com>
2021-07-10 14:05:16 +0900

    update schema

--- server/schema.js
+++ server/schema.js
@@ -1,6 +1,15 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type City {
+    name: String!
+    specialties: [Specialty]
+    sights: [Sight]
+    population: Int
+    region: Region
+    teams: [Team]
+  }
+
   type Specialty {
     name: String!
     description: String
@@ -9,15 +18,6 @@ const typeDefs = gql`
   type Sight {
     name: String!
     description: String
-  }
-
-  type City {
-    name: String!
-    specialties: [Specialty]
-    sights: [Sight]
-    population: Int
-    region: Region
-    teams: [Team]
   }
 
   type Region {
@@ -31,11 +31,11 @@ const typeDefs = gql`
     league: [League]
   }
 
-  type League {
+  type Sport {
     name: String!
   }
 
-  type Sport {
+  type League {
     name: String!
   }
 
@@ -44,6 +44,15 @@ const typeDefs = gql`
     getCities: [City]
     getTeamsByCity(name: String!): [Team]
     getCityByTeam(name: String!): [City]
+  }
+
+  input CityInput {
+    name: String!
+    specialties: [SpecialtyInput]
+    sights: [SightInput]
+    population: Int
+    region: RegionInput
+    teams: [TeamInput]
   }
 
   input SpecialtyInput {
@@ -56,31 +65,12 @@ const typeDefs = gql`
     description: String
   }
 
-  input CityInput {
-    name: String!
-    specialties: [SpecialtyInput]
-    sights: [SightInput]
-    population: Int
-    region: [String]
-    teams: [TeamInput]
+  input RegionInput {
+    name: String
   }
 
   input TeamInput {
     name: String!
-    sport: String
-    hometown: String
-    league: [String]
-  }
-
-  input NewTeam {
-    name: String!
-    sport: String
-    hometown: String
-    league: [String]
-  }
-
-  input UpdateTeam {
-    name: String
     sport: String
     hometown: String
     league: [String]
@@ -91,8 +81,12 @@ const typeDefs = gql`
     specialties: [SpecialtyInput]
     sights: [SightInput]
     population: Int
-    region: [String]
+    region: RegionInput
     teams: [TeamInput]
+  }
+
+  input BasicCity {
+    name: String!
   }
 
   input UpdateCity {
@@ -104,14 +98,36 @@ const typeDefs = gql`
     teams: [TeamInput]
   }
 
-  # type Mutation {
-  #   addTeam(input: NewTeam!) Team
-  #   updateTeam(name: String! input: UpdateTeam!) Team
-  #   deleteTeam(name: String!) [Team]
-  #   addCity(input: NewCity!) City
-  #   updateCity(name: String! input: UpdateCity!) City
-  #   deleteCity(name: String!) [City]
-  # }
+  input NewTeam {
+    name: String!
+    sport: String
+    hometown: String
+    league: [String]
+  }
+
+  input UpdateSpecialty {
+    name: String
+    description: String
+  }
+
+  input UpdateTeam {
+    name: String
+    sport: String
+    hometown: String
+    league: [String]
+  }
+
+  type Mutation {
+    #   addTeam(input: NewTeam!) Team
+    #   updateTeam(name: String! input: UpdateTeam!) Team
+    #   deleteTeam(name: String!) [Team]
+    # addCity(input: BasicCity!): City
+    #   updateCity(name: String! input: UpdateCity!) City
+    #   deleteCity(name: String!) [City]
+    addSpecialty(name: String!, description: String): Specialty
+    updateSpecialty(name: String!, input: UpdateSpecialty!): Specialty
+    deleteSpecialty(name: String!): Specialty
+  }
 `;
 
 module.exports = typeDefs;
