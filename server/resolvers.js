c920e9a4ec708e5f46a1d178d59fac83f78c889a
Conall Imlah <conallimlah@gmail.com>
2021-07-10 14:05:41 +0900

    add create, update, delete resolvers for specialty

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,5 @@
+const { isConditionalExpression } = require("typescript");
+
 module.exports = {
   Query: {
     getTeams: async (_parent, _args, context) => {
@@ -39,7 +41,7 @@ module.exports = {
       });
     },
     getCityByTeam: async (_parent, { name }, context) => {
-      return context.prisma.city.findMany({
+      return context.prisma.city.findUnique({
         where: {
           teams: {
             some: {
@@ -61,6 +63,88 @@ module.exports = {
           },
         },
       });
+    },
+  },
+  Mutation: {
+    // addCity: async (_parent, { input }, context) => {
+    //   console.log(input);
+    //   await context.prisma.city.create({
+    //     data: {
+    //       name: input.name,
+    //     },
+    //   });
+    // await context.prisma.city.create({
+    //   data: {
+    //     name: input.name,
+    //     specialties: {
+    //       create: [
+    //         {
+    //           name: input.specialties.name,
+    //           description: input.specialties.description,
+    //         },
+    //       ],
+    //     },
+    //     sights: {
+    //       create: [
+    //         {
+    //           name: input.sights.name,
+    //           description: input.sights.description,
+    //         },
+    //       ],
+    //     },
+    //     population: input.population,
+    //     region: {
+    //       create: [{ name: input.region.name }],
+    //     },
+    //     teams: {
+    //       create: [
+    //         {
+    //           name: input.teams.name,
+    //           sport: {
+    //             create: [{ name: input.teams.sport }],
+    //           },
+    //           league: {
+    //             create: [{ name: input.teams.league }],
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   },
+    // });
+    // return context.prisma.city.findUnique({
+    //   where: {
+    //     name: input.name,
+    //   },
+    // });
+    // },
+    addSpecialty: async (_parent, args, context) => {
+      const created = await context.prisma.specialty.create({
+        data: {
+          name: args.name,
+          description: args.description,
+        },
+      });
+      return created;
+    },
+    updateSpecialty: async (_parent, args, context) => {
+      const updated = await context.prisma.specialty.update({
+        where: {
+          name: args.name,
+        },
+        data: {
+          name: args.input?.name,
+          description: args.input?.description,
+        },
+      });
+      return updated;
+    },
+    deleteSpecialty: async (_parent, { name }, context) => {
+      const deleted = await context.prisma.specialty.delete({
+        where: {
+          name: name,
+        },
+      });
+      return deleted;
     },
   },
 };
