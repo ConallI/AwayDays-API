8f1a7eb501f1dd0b47891a7b9a085f16a2e8986d
Conall <conallimlah@gmail.com>
2021-11-05 00:33:42 +0900

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended"],
+  extends: ["plugin:react/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
